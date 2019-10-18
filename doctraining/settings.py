9be974cd6cb2968185e4072cb103543ef4dd5f62
Jesaias <jesaias.silva@alunos.ufersa.edu.br>
2019-10-18 20:06:59 -0300

    banco_uol

--- doctraining/settings.py
+++ doctraining/settings.py
@@ -101,27 +101,28 @@ WSGI_APPLICATION = 'doctraining.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE':'mysql.connector.django',
-#         'HOST':'doctraining.mysql.uhserver.com',
-#         'PASSWORD':'docles2019*',
-#         'USER':'mod_inteligente',
-#         'NAME':'doctraining',
-#         'PORT':'3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE':  config('ENGINE'),
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'ENGINE':'mysql.connector.django',
+        'HOST':'doctraining.mysql.uhserver.com',
+        'PASSWORD':'docles2019*',
+        'USER':'mod_inteligente',
+        'NAME':'doctraining',
+        'PORT':'3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE':  config('ENGINE'),
+#         'ENGINE':'mysql.connector.django',
+#         'NAME': config('NAME'),
+#         'USER': config('USER'),
+#         'PASSWORD': config('PASSWORD'),
+#         'HOST': config('HOST'),
+#         'PORT': config('PORT'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
