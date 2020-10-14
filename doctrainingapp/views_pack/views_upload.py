af1d98f2ace1b064792eefd345527929fbb58ef9
jhom1310 <aleffjonathan7@gmail.com>
2020-10-14 14:28:01 -0300

    att api

--- doctrainingapp/views_pack/views_upload.py
+++ doctrainingapp/views_pack/views_upload.py
@@ -2,7 +2,6 @@ from doctrainingapp.views import *
 #DECORATORS
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-
 import csv, io
 from django.shortcuts import render
 from django.contrib import messages
